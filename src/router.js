3d9d651bcf8c8d15ff574686281298b384131935
momise92 <boss973@hotmail.fr>
2019-03-04 14:45:13 +0100

    add router push if is login

--- src/router.js
+++ src/router.js
@@ -29,6 +29,10 @@ export default new Router({
         {
             path: '/inscription',
             name: 'inscription',
+            beforeEnter: (to, from, next) => {
+                if (auth.getLocalToken()) next("/profil");
+                else next();
+            },
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
